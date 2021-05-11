5fdb0fb2233d65a57215b7a68bf32fb1e8e2a4e0
Olha Shevel <o.v.shevel@gmail.com>
2021-05-11 17:06:52 +0300

    Algotirhm desc finished, search tree structure added

---
+++ src/com/company/searchTree.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class searchTree {
+    stateNode root;
+
+    searchTree(stateNode root){
+        this.root = root;
+    }
+
+    public void generateChildren(){
+
+    }
+}
