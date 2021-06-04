68329eb572b26cb35dc89a2e58831ca7f62d1b8d
Olha Shevel <o.v.shevel@gmail.com>
2021-06-04 09:12:15 +0300

    Comments" d

--- src/com/company/EightQueenPanel.java
+++ src/com/company/EightQueenPanel.java
@@ -13,7 +13,7 @@ class EightQueenPanel extends JPanel {
     private String str = "Insert queens";
     private String algorithm;
     private JPanel gui;
-    private int count = 0;
+    private int count = 0; //кількість королев на дошці
     private JPanel board;
     Solution solution;
     JRadioButton r1;
@@ -27,13 +27,8 @@ class EightQueenPanel extends JPanel {
     private ArrayList<Integer> inputArray = new ArrayList<>();
     ArrayList<ArrayList<Integer>> solutionArray = new ArrayList<>();
     int depth = 8;
-    int index = 0;
+    int index = 0; //for timer
 
-/*TODO:
-1. radioButtons for methods (BFS, LDFS, IDS)
-2. textArea for depth for LDFS
-3. remove rastavlyau korolev
-* **/
 
     public EightQueenPanel() {
         setLayout(new FlowLayout(FlowLayout.CENTER));
@@ -41,10 +36,10 @@ class EightQueenPanel extends JPanel {
         timer = new Timer(500, new SubmitListener());
         gui = new JPanel();
         board = createBoard();
+        gui.add(board);
         ActionListener submitListener = new SubmitListener();
         ActionListener resetListener = new ResetListener();
         add(gui);
-
         textArea = new JTextArea(str, 9,25);
         textArea1 = new JTextArea("8");
         textArea.setFont(new Font("Book Antiqua", Font.PLAIN, 14));
@@ -52,17 +47,13 @@ class EightQueenPanel extends JPanel {
         scr = new JScrollPane(textArea);
         textArea.setLineWrap(true);
         textArea1.setLineWrap(true);
-
         add(scr);
 
         JPanel buttons =new JPanel();
         buttons.setLayout (new BoxLayout (buttons, BoxLayout.Y_AXIS));
-        //buttons.setSize(200,200);
         r1 = new JRadioButton("BFS",true);
         r2 = new JRadioButton("IDS");
         r3 = new JRadioButton("LDFS");
-        r1.setBounds(20,20,80,30);
-        r2.setBounds(50,100, 80,30);
         ButtonGroup rbGroup = new ButtonGroup();
         rbGroup.add(r1);
         rbGroup.add(r2);
@@ -74,19 +65,10 @@ class EightQueenPanel extends JPanel {
         buttons.add(r3);
         buttons.add(new JLabel("LDFS Depth"));
         buttons.add(textArea1);
-
-
-
-
-
-
         add(submitButton);
         submitButton.addActionListener(submitListener);
         add(resetButton);
         resetButton.addActionListener(resetListener);
-        gui.add(board);
-
-
     }
 
     public class SubmitListener implements ActionListener {
@@ -108,27 +90,30 @@ class EightQueenPanel extends JPanel {
             if (algorithm.equals("LDFS")) {
 
                 String strDepth = textArea1.getText();
-                try {depth = Integer.parseInt(strDepth);}
-                catch (Exception e) {
+                try {
+                    depth = Integer.parseInt(strDepth);
+                } catch (Exception e) {
                     textArea.setText("Invalid depth value. Please enter int > 0.");
                     return;
                 }
-
-                try {solution = new Solution(inputArray, algorithm, depth);
+                if (depth < 0) {
+                    textArea.setText("Invalid depth value. Please enter int > 0.");
+                    return;
+                }
+                try {
+                    solution = new Solution(inputArray, algorithm, depth);
 
                 } catch (CloneNotSupportedException e) {
                     e.printStackTrace();
                 }
 
             } else
-                try {solution = new Solution(inputArray, algorithm);
+                try {
+                    solution = new Solution(inputArray, algorithm);
 
                 } catch (CloneNotSupportedException e) {
                     e.printStackTrace();
                 }
-
-
-
             if(!solution.isFound) {
                 textArea.setText(solution.getTraversal());
                 inputArray.clear();
@@ -144,16 +129,16 @@ class EightQueenPanel extends JPanel {
             File pic = new File("src/com/company/queen.png");
             ImageIcon icon = new ImageIcon(pic.toString());
             icon.setImage(icon.getImage().getScaledInstance(80,80,Image.SCALE_DEFAULT));
-
+//timer starts to work
             if (index < solutionArray.size()) {
                 timer.start();
             }
 
-            for (int i = 1; i < 65; i++) {
+            for (int i = 1; i < 65; i++) { // clear board
                 buttons[i].setIcon(null);
             }
 
-            for (int i = 0; i < 8; i++) {
+            for (int i = 0; i < 8; i++) { // fill solution into buttons array
                 buttons[solutionArray.get(index).get(i)].setIcon(icon);
                 System.out.println(solutionArray.get(index).get(i));
             }
@@ -165,7 +150,7 @@ class EightQueenPanel extends JPanel {
             count = 8;
             gui.remove(board);
             board = newBoard;
-            //textArea.append(str);
+
             gui.add(board);
             revalidate();
             repaint();
@@ -198,6 +183,7 @@ class EightQueenPanel extends JPanel {
 
     public JPanel createBoard() {
         count = 0;
+
         class ClickListener implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent event) {
